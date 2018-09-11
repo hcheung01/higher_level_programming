0fee2de477e13c13cbd1368a560fb20116fd5645
Heindrick Cheung <hcheung01@gmail.com>
2018-09-11 23:40:13 +0000

    print last digit of numbers

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/bin/usr/python3
+def print_last_digit(number):
+    if number < 0:
+        final = ((number * -1) % 10)
+    elif number > 0:
+        final = number % 10
+    else:
+        final = number
+    print("{:d}".format(final), end="")
+    return final
