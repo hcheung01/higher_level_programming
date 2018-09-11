10f21d3793f8028d052b8f4cdafee2722aec9524
Heindrick Cheung <hcheung01@gmail.com>
2018-09-11 20:57:47 +0000

    checker for negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = number % 10
 greater = "and is greater than 5"
 lesser = "and is less than 6 and not 0"
 equal = "and is 0"
+
+if number > 0:
+    last = number % 10
+else:
+    last = ((number * -1) % 10) * -1
+
 if last > 5:
     final = greater
-elif last < 6 and not 0:
-    final = lesser
 elif last is 0:
     final = equal
+elif last < 6 and not 0:
+    final = lesser
 print("Last digit of", number, "is", last, final)
