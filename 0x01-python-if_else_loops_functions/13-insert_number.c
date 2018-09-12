428757f9855a64d36d89f8ef3975a86041a3b258
Heindrick Cheung <hcheung01@gmail.com>
2018-09-12 00:47:54 +0000

    check for value of linked list and insert in sorted order of n value

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * insert_node - function with two arguments
+ * @head: head pointer to linked list
+ * @number: input number
+ *
+ * Description: insert n value to sorted linked list
+ * Return: address of new node, or NULL if fail
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new, *current, *ahead;
+
+	current = *head;
+	ahead = current->next;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (*head == NULL)
+		*head = new;
+	else
+	{
+		while (ahead->next != NULL)
+		{
+			if (number > current->n && number < ahead->n)
+			{
+				current->next = new;
+				new->next = ahead;
+			}
+			current = current->next;
+			ahead = ahead->next;
+		}
+	}
+	return (new);
+}
