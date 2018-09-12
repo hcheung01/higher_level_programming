71f82e4c39117aaab35ced8ccdec7562019d8188
Heindrick Cheung <hcheung01@gmail.com>
2018-09-12 18:13:49 +0000

    create a copy of string argument without n character

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/bin/usr/python3
+def remove_char_at(str, n):
+    copy = ""
+    count = 0
+    for i in str:
+        if count is not n:
+            copy += i
+        count = count + 1
+    return copy
