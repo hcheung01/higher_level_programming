6816562bc7527e40369d079c2b9b0d55b4ee84c3
Heindrick Cheung <hcheung01@gmail.com>
2018-09-13 23:53:29 +0000

    can take input from argv to add, sub, mul and div with a return input with error checking

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    from sys import argv
+
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    op = argv[2]
+    num1 = int(argv[1])
+    num2 = int(argv[3])
+
+    if op is '+':
+        print("{:d} + {:d} = {:d}".format(num1, num2, add(num1, num2)))
+    elif op is '-':
+        print("{:d} + {:d} = {:d}".format(num1, num2, sub(num1, num2)))
+    elif op is '*':
+        print("{:d} + {:d} = {:d}".format(num1, num2, mul(num1, num2)))
+    elif op is '/':
+        print("{:d} + {:d} = {:d}".format(num1, num2, div(num1, num2)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
