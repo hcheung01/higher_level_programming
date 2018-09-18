601a5c8c9bf97e266cc56ec44adbb699f271b537
Heindrick Cheung <hcheung01@gmail.com>
2018-09-18 22:37:59 +0000

    copy a list and insert new element at index

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx >= 0 and idx <= len(my_list) and my_list:
+        new_list = my_list.copy()
+        new_list[idx] = element
+        return (new_list)
+    return (my_list)
