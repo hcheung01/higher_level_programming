54707705c869ae603a222e450975793fc59cb43b
Heindrick Cheung <hcheung01@gmail.com>
2018-09-18 22:00:03 +0000

    check for less than length of list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx >= 0 and idx <= len(my_list):
+    if idx >= 0 and idx < len(my_list):
         return (my_list.pop(idx))
     else:
         return (None)
