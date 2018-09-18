eadcbe52bcb4b55f44a1456640678326ec48c530
Heindrick Cheung <hcheung01@gmail.com>
2018-09-18 22:42:18 +0000

    change if condition

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     new_list = my_list.copy()
-    if idx >= 0 and idx <= len(my_list) and my_list:
+    if idx >= 0 and idx <= len(my_list):
         new_list[idx] = element
         return (new_list)
-    return (new_list)
+    return (my_list)
