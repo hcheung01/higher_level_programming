9697f7da79eb34ef44b7cf1ec26575ac702f0086
Heindrick Cheung <hcheung01@gmail.com>
2018-09-19 03:20:30 +0000

    return a tuple with length of string and 1 char

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if not sentence:
+        tup = tuple((0, none))
+    else:
+        tup = tuple((len(sentence), sentence[0]))
+    return (tup)
