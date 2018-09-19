415cf31d73de4211c1a99c53155a73d25bc77574
Heindrick Cheung <hcheung01@gmail.com>
2018-09-19 03:04:00 +0000

    add elements at same index of two different tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tup = ()
+    if len(tuple_b) < 1:
+        tuple_b = tuple_b + (0, 0)
+    elif len(tuple_b) < 2:
+        tuple_b = tuple_b + (0,)
+    for i, j in zip(tuple_a, tuple_b):
+        sum = i + j
+        tup = tup + (sum,)
+    return (tup)
