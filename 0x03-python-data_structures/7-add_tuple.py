3b44cc126e2479e007570ab1ac92c4135ff09555
Heindrick Cheung <hcheung01@gmail.com>
2018-09-19 03:14:57 +0000

    added checker for tuple a

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
     tup = ()
+    if len(tuple_a) < 1:
+        tuple_a = tuple_a + (0, 0)
+    elif len(tuple_a) < 2:
+        tuple_a = tuple_a + (0,)
     if len(tuple_b) < 1:
         tuple_b = tuple_b + (0, 0)
     elif len(tuple_b) < 2:
