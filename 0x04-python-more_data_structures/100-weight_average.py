f0295de7be1d7a2e467092cafd033c222bec5b6b
Heindrick Cheung <hcheung01@gmail.com>
2018-09-21 06:56:42 +0000

    average tuple

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    if my_list:
-        return (sum([x * y for x, y in my_list]) / sum(y for x, y in my_list))
-    return None
+    if not my_list:
+        return 0
+    return (sum([x * y for x, y in my_list]) / sum(y for x, y in my_list))
