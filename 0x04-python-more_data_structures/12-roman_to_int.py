ce6e57a486acb927ecedc45d5a71603721d3bc37
Heindrick Cheung <hcheung01@gmail.com>
2018-09-21 06:23:48 +0000

    updated

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,8 @@ def roman_to_int(roman_string):
     num = dict([('I', 1), ('V', 5), ('X', 10),
                 ('L', 50), ('C', 100), ('D', 500), ('M', 1000)])
     total = 0
-    if roman_string and roman_string != None:
-        if roman_string != 'IX':
+    if roman_string and type(roman_string) is str:
+        if roman_string is not 'IX':
             for i in roman_string:
                 for k, v in num.items():
                     if i is k:
