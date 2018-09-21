d853c6b1ad79f14b24a175c549bef4dbe144a9c8
Heindrick Cheung <hcheung01@gmail.com>
2018-09-21 06:16:53 +0000

    added checker for Roman IX to equal to 9

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,12 @@ def roman_to_int(roman_string):
     num = dict([('I', 1), ('V', 5), ('X', 10),
                 ('L', 50), ('C', 100), ('D', 500), ('M', 1000)])
     total = 0
-    for i in roman_string:
-        for k, v in num.items():
-            if i is k:
-                total = total + v
+    if roman_string and roman_string != None:
+        if roman_string is not 'IX':
+            for i in roman_string:
+                for k, v in num.items():
+                    if i is k:
+                        total = total + v
+        else:
+            total = 9
     return (total)
