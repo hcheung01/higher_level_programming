547749b0dd39d98769416f7eb7d8e61fd055f87d
Heindrick Cheung <hcheung01@gmail.com>
2018-09-22 21:24:12 +0000

    added whitespace after : in tuple

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if type(roman_string) is not str or roman_string is None:
         return 0
-    nums = {'M':1000, 'D':500, 'C':100, 'L':50, 'X':10, 'V':5, 'I':1}
+    nums = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
     sum = 0
     for i in range(len(roman_string)):
         value = nums[roman_string[i]]
