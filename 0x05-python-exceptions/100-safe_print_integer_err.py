796e97a9a9b3ce6f38eb263d2793f5ee6cc3e93c
Heindrick Cheung <hcheung01@gmail.com>
2018-10-02 04:34:01 +0000

    changed from ValueError to Exception

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,6 +4,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError as err:
+    except Exception as err:
         print("Exception: {}".format(err), file=stderr)
         return False
