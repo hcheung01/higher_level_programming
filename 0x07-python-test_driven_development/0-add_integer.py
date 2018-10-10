4202978c3d85cd49b50e738233c1a8b7fd5c55c1
Heindrick Cheung <hcheung01@gmail.com>
2018-10-10 15:54:37 +0000

    pep8 fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Module to sum
-
-add_integer: adds two integers and return
-
 """
+Module to sum
+add_integer: adds two integers and return
+"""
+
 
 def add_integer(a, b=98):
     """Return sum of a + b
     Args: a = int/float b = int/float
     """
+
     if type(a) != int and type(a) != float:
         raise TypeError('a must be an integer')
     if type(b) != int and type(b) != float:
