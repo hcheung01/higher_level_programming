b57b1c28e8a6fdd999b770343c39212e123ed88a
Heindrick Cheung <hcheung01@gmail.com>
2018-10-17 03:44:22 +0000

    update

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -15,6 +15,6 @@ def read_lines(filename="", nb_lines=0):
     line_num = 0
     with open(filename, encoding="utf-8") as f:
         for line in f:
-            if line_num < nb_lines or not nb_lines:
+            if line_num < nb_lines or not nb_lines or nb_lines < 0:
                 print(line, end="")
             line_num += 1
