15db88cdac365f92e5efc7bb8c548e838a6d8615
Heindrick Cheung <hcheung01@gmail.com>
2018-10-17 03:49:54 +0000

    update

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -22,7 +22,7 @@ class Student:
             dictionary
         """
         if not attrs:
-            return self.__dict__
+            return (self.__dict__)
 
         return ({key: value for key, value in self.__dict__.items()
                  if key in attrs})
