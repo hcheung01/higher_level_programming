50ff3634ef27ea4413e3021ed34d9bb0b64e5f9e
Heindrick Cheung <hcheung01@gmail.com>
2018-10-17 06:01:26 +0000

    update

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -5,11 +5,13 @@ Class Module
 
 
 class Student:
-    """Student class
+    """
+    Student class
     """
 
     def __init__(self, first_name, last_name, age):
-        """initialize method"""
+        """initialize method
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -22,7 +24,7 @@ class Student:
             dictionary
         """
         if not attrs:
-            return (self.__dict__)
+            return self.__dict__
 
         return ({key: value for key, value in self.__dict__.items()
                  if key in attrs})
