2739efd915c954073bfbabe31878fb9bcb6bdf62
Heindrick Cheung <hcheung01@gmail.com>
2018-12-04 07:59:32 +0000

    change database, table to utf8 format

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,3 @@
+-- Convert database, table and specific field to UTF-8
+ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE `hbtn_0c_0`.`first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
