aa813e6f9b522706fa598aa4688e06c6c42115e6
Heindrick Cheung <hcheung01@gmail.com>
2018-12-05 09:07:30 +0000

    match show to genre

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- List all shows and all genres linked to a show
 SELECT a.title, c.name
 FROM tv_shows AS a
-JOIN tv_show_genres AS b
+LEFT JOIN tv_show_genres AS b
 ON a.id = b.show_id
-JOIN tv_genres AS c
+LEFT JOIN tv_genres AS c
 ON b.genre_id = c.id
 ORDER BY a.title ASC;
