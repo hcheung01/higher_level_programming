b2ac153c6aa4f0ffa3e0b7ce857ec1c7240dc2e1
Heindrick Cheung <hcheung01@gmail.com>
2018-12-05 17:29:48 +0000

    find not matching genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,9 +1,12 @@
 -- List all genres not linked to the show Dexter
-SELECT c.name
-FROM tv_shows AS a
-JOIN tv_show_genres AS b
-ON a.id = b.show_id
-JOIN tv_genres AS c
-ON b.genre_id = c.id
-WHERE a.title = 'Dexter'
-ORDER BY c.name ASC;
+SELECT tv_genres.name
+FROM tv_genres
+WHERE tv_genres.name NOT IN
+      (SELECT a.name AS name
+      FROM tv_genres AS a
+      JOIN tv_show_genres AS b
+      ON b.genre_id = a.id
+      JOIN tv_shows AS c
+      ON b.show_id = c.id
+      WHERE c.title = 'Dexter'
+      ORDER BY a.name ASC);
