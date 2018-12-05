46098ec5526e11c0d5b9afe5a303c0932ed47f2e
Heindrick Cheung <hcheung01@gmail.com>
2018-12-05 08:30:02 +0000

    show all genre of a show

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,9 @@
+-- Uses a database to lists all genres a show
+SELECT a.name AS name
+FROM tv_genres AS a
+JOIN tv_show_genres AS b
+ON b.genre_id = a.id
+JOIN tv_shows AS c
+ON b.show_id = c.id
+WHERE c.title = 'Dexter'
+ORDER BY a.name ASC;
