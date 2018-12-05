8372b4cc6162e16caa331be0a33c389462e05680
Heindrick Cheung <hcheung01@gmail.com>
2018-12-05 08:44:32 +0000

    list all comedy genre shows

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- List all Comedy shows in a database
+SELECT a.title
+FROM tv_shows AS a
+JOIN tv_show_genres AS b
+ON a.id = b.show_id
+JOIN tv_genres AS c
+ON b.genre_id = c.id
+WHERE c.name = 'Comedy'
+ORDER BY a.title ASC;
