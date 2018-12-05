518efd1632f2acb32d691a88ababb327a5cf54c7
Heindrick Cheung <hcheung01@gmail.com>
2018-12-05 06:40:31 +0000

    change order by to ASC

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,6 +1,6 @@
 -- Lists all the cities of California found in a database
 SELECT cities.id, cities.name
-FROM hbtn_0d_usa.cities, hbtn_0d_usa.states
+FROM cities, states
 WHERE cities.state_id = states.id
 AND states.name = 'California'
-ORDER BY cities.id;
+ORDER BY cities.id ASC;
