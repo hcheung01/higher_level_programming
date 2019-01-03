21e1e0f67eb623f2b25ceb61009d6a9e7a3936aa
Heindrick Cheung <hcheung01@gmail.com>
2019-01-03 21:06:28 +0000

    updated to pep8 styling guideline

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,11 @@ if __name__ == "__main__":
     from sys import argv
 
     # connect
-    db = MySQLdb.connect(host='localhost', port=3306, user=argv[1], passwd=argv[2], db=argv[3])
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
 
     # cursor
     c = db.cursor()
