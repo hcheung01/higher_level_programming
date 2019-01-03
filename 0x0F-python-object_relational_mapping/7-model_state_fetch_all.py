6b07459160f475282cad8661ab29ac466d425eb6
Heindrick Cheung <hcheung01@gmail.com>
2019-01-03 23:46:57 +0000

    find column in a table after connecting to database

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# list all state objects from the database
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy.engine.url import URL
+
+
+    db = {'drivername': 'mysql+mysqldb',
+          'host': 'localhost',
+          'port': '3306',
+          'username': argv[1],
+          'password': argv[2],
+          'database': argv[3]}
+
+    url = URL(**db)
+    engine = create_engine(url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    for state in session.query(State).order_by(State.id).all():
+        print("{}".format(state.name))
+
+    session.close()
