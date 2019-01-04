fb5b1112fd238e8964970be96f5e46b90339554c
Heindrick Cheung <hcheung01@gmail.com>
2019-01-04 00:06:40 +0000

    query for one name

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# print first state objects from the database
+
+if __name__ == "__main__":
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    from sys import argv
+    from model_state import Base, State
+    from sqlalchemy.engine.url import URL
+
+    # url
+    url = {'drivername': 'mysql+mysqldb',
+           'host': 'localhost',
+           'port': '3306',
+           'username': argv[1],
+           'password': argv[2],
+           'database': argv[3]}
+
+    c_url = URL(**url)
+
+    # create engine, metadata for stored objects
+    engine = create_engine(c_url, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    # session
+    session = Session(engine)
+
+    # query and print
+    for state in session.query(State).filter(State.id == 1):
+        print("{}: {}".format(state.id, state.name))
+
+    # close
+    session.close()
