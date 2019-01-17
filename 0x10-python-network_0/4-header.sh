8e2992f1e929e71ef698d2d35541ce3e2178662e
Heindrick Cheung <hcheung01@gmail.com>
2019-01-17 03:22:27 +0000

    update

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a GET request to the URL and displays the body of the response
-curl -sX GET "$1" -d "X-HolbertonSchool-User-Id=98"
+curl -sX GET "$1" -H "X-HolbertonSchool-User-Id=98"
