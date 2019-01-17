6f56fdcf045c44800a60d39a87ba24431814b739
Heindrick Cheung <hcheung01@gmail.com>
2019-01-17 00:14:04 +0000

    added grep first

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Takes in a URL, sends a request to that URL and display the size
-curl -sI "$1" | awk '$1 == "Content-Length:" {print $2}'
+curl -sI "$1" | grep Content-Length | awk '$1 == "Content-Length:" {print $2}'
