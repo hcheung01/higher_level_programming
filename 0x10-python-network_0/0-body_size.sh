e258223a47197c01a2d681707d61eed23e198c8d
Heindrick Cheung <hcheung01@gmail.com>
2019-01-17 00:09:06 +0000

    shellchecked

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
-# Takes in a URL, sends a request to that URL
-# and display the size of body of response
-curl -sI $1 | awk '$1 == "Content-Length:" {print $2}'
+#!/bin/bashxc
+# Takes in a URL, sends a request to that URL and display the size
+curl -sI "$1" | awk '$1 == "Content-Length:" {print $2}'
