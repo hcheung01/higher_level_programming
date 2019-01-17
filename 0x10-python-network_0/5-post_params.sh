3183bbe84b56dc4973f86a28acc7763ce8b62267
Heindrick Cheung <hcheung01@gmail.com>
2019-01-17 03:40:07 +0000

    update

---
+++ 0x10-python-network_0/5-post_params.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# takes in a URL sends a POST request to passed URL and displays response body
+curl -s "$1" -X POST -d \
+    "email:hr@holbertonschool.com&subject:I will always be here for PLD"
