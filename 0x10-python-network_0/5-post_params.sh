a36874742cd39b3649e9a2f11ed68ee21de59cec
Heindrick Cheung <hcheung01@gmail.com>
2019-01-17 03:40:54 +0000

    reduce to one line

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # takes in a URL sends a POST request to passed URL and displays response body
-curl -s "$1" -X POST -d \
-    "email:hr@holbertonschool.com&subject:I will always be here for PLD"
+curl -s "$1" -X POST -d "email:hr@holbertonschool.com&subject:I will always be here for PLD"
