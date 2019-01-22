ed36df361fcbc68f31c2f12a1920a67d2f8f1ee9
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 04:52:07 +0000

    send request and print error code

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+"""
+script to take in a URL input, sends a request to the URL, display response
+"""
+
+
+if __name__ == '__main__':
+    from sys import argv
+    import requests
+
+    try:
+        req = requests.get(argv[1])
+        if req:
+            print(req.text)
+        elif req.status_code >= 400:
+            print("Error code: {}".format(req.status_code))
+    except KeyError:
+        pass
