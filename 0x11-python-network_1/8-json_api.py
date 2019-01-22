2f2706fa3f5171763fcc7b1ec44d1bbbbaba9f81
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 06:18:15 +0000

    POST request with a letter parameter in json format

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Script that takes in a letter and sends a POST request to URL with letter param
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import requests
+
+    if len(argv) == 1:
+        q = ''
+    else:
+        q = argv[1]
+
+    r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        out = r.json()
+        if json:
+            print('[{}] {}'.format(out['id'], out['name']))
+        else:
+            print('No result')
+    except:
+        print('Not a valid JSON')
