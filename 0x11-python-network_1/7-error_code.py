b72524f913da845aa89e45a18f9820d6a173f7af
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 05:02:20 +0000

    simplify my logic and refactored code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,12 +8,8 @@ if __name__ == '__main__':
     from sys import argv
     import requests
 
-    try:
-        req = requests.get(argv[1])
-        if req.status_code == 200:
-            print(req.text)
-    except KeyError:
-        pass
+    req = requests.get(argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
     else:
-        if req.status_code >= 400:
-            print("Error code: {}".format(req.status_code))
+        print(page.text)
