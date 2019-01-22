60b718f5d0f5abcc87506aae882a56ef46449f8c
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 02:37:47 +0000

    will not execute when imported and fixed pep8 extra blank line

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,13 +2,15 @@
 """
 Python script
 """
-from urllib.request import Request, urlopen
 
 
-req = Request('https://intranet.hbtn.io/status')
-with urlopen(req) as f:
-    the_page = f.read()
-print('Body response:$')
-print('\t- type: {}$'.format(type(the_page)))
-print('\t- content: {}$'.format(the_page))
-print('\t- utf8 content: {}$'.format(the_page.decode('utf8')))
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+
+    req = Request('https://intranet.hbtn.io/status')
+    with urlopen(req) as f:
+        the_page = f.read()
+        print('Body response:$')
+        print('\t- type: {}$'.format(type(the_page)))
+        print('\t- content: {}$'.format(the_page))
+        print('\t- utf8 content: {}$'.format(the_page.decode('utf8')))
