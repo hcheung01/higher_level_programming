a3df77547639ffaab5116260529b331e502a77e7
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 03:48:02 +0000

    script for sending request to a command line inputted URL and displays the body of the response decoded in utf8 or catch HTTPError

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Python script
+"""
+
+
+if __name__ == '__main__':
+    from urllib.request import Request, urlopen
+    from urllib.error import HTTPError
+    from sys import argv
+
+    req = Request(argv[1])
+    try:
+        with urlopen(req) as f:
+            response = f.read()
+            print(response.decode('utf8'))
+    except HTTPError as e:
+        print(e.code)
