9780f717200cc95e086a07aee6b1263db676324a
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 07:33:17 +0000

    basic authentication with requests to github api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes my Github credentials and uses the Github API to display my id
+"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    try:
+        r = requests.get("https://api.github.com/user", auth=(argv[1], argv[2]))
+        file = r.json()
+        print(file['id'])
+    except:
+        print('None')
