b3ca5078c35d7d072b5eb9a54e37ec7ba859b069
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 04:06:03 +0000

    display output was not correct, needed to print string from text method instead of content

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.content))
+    print("\t- content: {}".format(r.text))
