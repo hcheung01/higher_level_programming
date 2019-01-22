e144bbc63beb0787ed317ae951a34dd39426017f
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 05:30:28 +0000

    print content and decode

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,9 @@ if __name__ == '__main__':
 
     try:
         req = requests.get(argv[1])
-        print(req.text)
-    except requests.exceptions.HTTPError:
-        print("Error code: {}".format(req.status_code))
+        if req:
+            print(req.content.decode('utf-8'))
+        elif req.status_code >= 400:
+            print("Error code: {}".format(req.status_code))
+    except KeyError:
+        pass
