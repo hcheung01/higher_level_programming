591fe9fe98393eb8b0e79111c786d8eca4325969
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 02:39:24 +0000

    removed $

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     req = Request('https://intranet.hbtn.io/status')
     with urlopen(req) as f:
         the_page = f.read()
-        print('Body response:$')
-        print('\t- type: {}$'.format(type(the_page)))
-        print('\t- content: {}$'.format(the_page))
-        print('\t- utf8 content: {}$'.format(the_page.decode('utf8')))
+        print('Body response:')
+        print('\t- type: {}'.format(type(the_page)))
+        print('\t- content: {}'.format(the_page))
+        print('\t- utf8 content: {}'.format(the_page.decode('utf8')))
