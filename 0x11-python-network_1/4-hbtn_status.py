345cef5c5bfbebf50f178ff28bf094a753b201b6
Heindrick Cheung <hcheung01@gmail.com>
2019-01-22 04:01:37 +0000

    using a new library 'requests' to manipulate requests

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Script to check status
+"""
+
+
+if __name__ == "__main__":
+    import requests
+
+    r = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.content))
