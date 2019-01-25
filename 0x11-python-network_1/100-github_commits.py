c51a5f5c2710943f3d4fa7e9e5df8993e1562a2d
Heindrick Cheung <hcheung01@gmail.com>
2019-01-25 19:41:23 +0000

    converted response object to json dictionary

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -13,9 +13,8 @@ if __name__ == '__main__':
         repo = argv[1]
         r = requests.get('https://api.github.com/repos/{}/{}/commits'
                          .format(name, repo))
-    if r:
-        for l in range(10):
-            dicts = r.json()[l]
-            author = dicts.get('commit').get('author').get('name')
-            sha = dicts.get('sha')
-            print("{}: {}".format(sha, author))
+        r = r.json()
+        for i in range(10):
+            name = r[i]['commit']['author']['name']
+            sha = r[i]['sha']
+            print("{}: {}".format(sha, name))
