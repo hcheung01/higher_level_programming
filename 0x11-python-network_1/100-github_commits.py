e1049b1fbb806d52ac89b416ba546a77c16ad72b
Heindrick Cheung <hcheung01@gmail.com>
2019-01-25 19:17:17 +0000

    chaged check condition from try/except block to if else

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -8,17 +8,14 @@ if __name__ == '__main__':
     import requests
     from sys import argv
 
-if (len(argv) == 3):
-    name = argv[2]
-    repo = argv[1]
-    r = requests.get('https://api.github.com/repos/{}/{}/commits'
-                     .format(name, repo))
-
-    for l in range(10):
-        try:
+    if (len(argv) == 3):
+        name = argv[2]
+        repo = argv[1]
+        r = requests.get('https://api.github.com/repos/{}/{}/commits'
+                         .format(name, repo))
+    if r:
+        for l in range(10):
             dicts = r.json()[l]
-        except:
-            break
-        author = dicts.get('commit').get('author').get('name')
-        sha = dicts.get('sha')
-        print("{}: {}".format(sha, author))
+            author = dicts.get('commit').get('author').get('name')
+            sha = dicts.get('sha')
+            print("{}: {}".format(sha, author))
