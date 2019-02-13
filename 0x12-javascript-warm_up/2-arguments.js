8a3b7eb16b56641ebe1f556a9fa94a9323079e17
Heindrick Cheung <hcheung01@gmail.com>
2019-02-13 05:11:36 +0000

    test argv input

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// script that prints a message depending of the number of arguments passed
+const test = process.argv;
+if (test.length < 3) { console.log('No argument'); } else if (test.length === 3) { console.log('Argument found'); } else { console.log('Argument founds'); }
