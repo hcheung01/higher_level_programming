17c21bc3bbd01640cf5bd14c3b2234881adac884
Heindrick Cheung <hcheung01@gmail.com>
2019-02-19 05:48:18 +0000

    print squares of class Rect

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// define class Rectangle version 3
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+    this.print = function () {
+      for (let j = 0; j < this.height; j++) {
+        console.log(Array(this.width + 1).join('X'));
+      }
+    };
+  }
+};
