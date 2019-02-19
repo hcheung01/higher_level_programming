1ee7682f65cc050db228acafc33403d0dc74775a
Heindrick Cheung <hcheung01@gmail.com>
2019-02-19 08:24:57 +0000

    shorted comments

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // prints the number of arguments already printed and the new argument value
-var num = 0;
+let num = 0;
 exports.logMe = function (item) {
   function print () {
     console.log(num + ': ' + item);
