de9fc93cfa7a960773649ce95051b6eb2121d6f0
Heindrick Cheung <hcheung01@gmail.com>
2019-02-20 14:34:29 -0800

    refactored algorithm and fixed semistandard format

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 const req = require('request');
-const id = parseInt(process.argv[2]);
-req('https://swapi.co/api/films', function (error, response, body) {
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+req(url, function (error, response, body) {
   if (error) { console.log(error); }
-  JSON.parse(body).results.forEach(function (movie) {
-    if (movie.episode_id === id) {
-      movie.characters.forEach(function (peeps) {
-        req(peeps, function (error, response, body) {
-          if (error) { console.log(error); }
-          console.log(JSON.parse(body).name);
-        });
-      });
-    }
+  JSON.parse(body).characters.forEach(function (peeps) {
+    req(peeps, function (error, response, body) {
+      if (error) { console.log(error); }
+      console.log(JSON.parse(body).name);
+    });
   });
 });
