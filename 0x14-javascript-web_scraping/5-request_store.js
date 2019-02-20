6990d7cc4e8f20f36eecc93dd293604c87c6a950
Heindrick Cheung <hcheung01@gmail.com>
2019-02-19 22:29:50 -0800

    request and pipe to file

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,6 @@
+#!/usr/local/bin/node
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const path = process.argv[3];
+request(url).pipe(fs.createWriteStream(path));
