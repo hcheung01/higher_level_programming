6312971c8762eabf01b7ea32b2281c11cb118ce8
Heindrick Cheung <hcheung01@gmail.com>
2019-02-19 21:30:02 -0800

    request Star Wars API and print title of found movie

--- 0x14-javascript-web_scraping/3-starwards_title.js
+++ 0x14-javascript-web_scraping/3-starwards_title.js
@@ -1,4 +1,4 @@
-#!/usr/local/bin/node
+#!/usr/bin/node
 const request = require('request');
 request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
   if (error) { console.log(error); }
