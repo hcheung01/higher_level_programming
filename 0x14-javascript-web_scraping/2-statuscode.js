4a367c58ce69b4b0efecd31a4aeed06f0a514d80
Heindrick Cheung <hcheung01@gmail.com>
2019-02-19 21:04:44 -0800

    fixed path to node

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -4,5 +4,5 @@ request(process.argv[2], function (error, response, body) {
   if (error) {
     console.log(error);
   }
-  console.log('code: ', response && response.statusCode);
+  console.log('code:', response && response.statusCode);
 });
