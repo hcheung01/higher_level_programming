d602d0f4dda68e89beccbe18eb8869a321924373
Heindrick Cheung <hcheung01@gmail.com>
2019-02-20 01:13:21 -0800

    prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/local/bin/node
+const req = require('request');
+const id = parseInt(process.argv[2]);
+req('https://swapi.co/api/films', function (error, response, body) {
+  if (error) { console.log(error); }
+  JSON.parse(body).results.forEach(function (movie) {
+    if (movie.episode_id === id) {
+      movie.characters.forEach(function (peeps) {
+        req(peeps, function (error, response, body) {
+          if (error) { console.log(error); }
+          console.log(JSON.parse(body).name);
+        });
+      });
+    }
+  });
+});
