8db358d32b53e920d8ef62edc4667bf2b9538bdc
Heindrick Cheung <hcheung01@gmail.com>
2019-02-19 23:58:54 -0800

    parse jsonplacer and return total userId

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,9 +1,9 @@
-#!/usr/bin/node
+#!/usr/local/bin/node
 const request = require('request');
 const url = process.argv[2];
 request(url, function (error, response, body) {
   if (error) { console.log(error); }
-  const ob = {};
+  let ob = {};
   body = JSON.parse(body);
   for (let i = 0; i < body.length; i++) {
     if (ob[body[i].userId]) {
@@ -11,6 +11,7 @@ request(url, function (error, response, body) {
     } else {
       ob[body[i].userId] = 1;
     }
+    console.log(body);
     console.log(ob);
   }
 });
