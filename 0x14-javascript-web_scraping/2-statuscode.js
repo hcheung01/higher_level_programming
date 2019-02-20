8eafced4cab0810a7545227675da2e6f2ab393ad
Heindrick Cheung <hcheung01@gmail.com>
2019-02-19 21:01:58 -0800

    request and print statuscode

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/local/bin/node
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
+  }
+  console.log('code: ', response && response.statusCode);
+});
